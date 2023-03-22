acdb55fc096dbb3f0c118fbfc5abd7b47c225553
Weenio <128557296+Weenio@users.noreply.github.com>
2023-03-22 20:36:27 -0300

    japoneis

---
+++ js/名前.js
@@ -0,0 +1,3 @@
+//"console.log" serve para mostrar uma mensagem no console. Ah, "気をつけろ" se lê "Kiwotsukero", que significa "Tome cuidado"!
+console.log('気をつけろ');
+//alert('Atenção, Atenção, essa página foi feita por um otaku fedido, prosiga por sua conta em risco 🤓🤓🤓🤓🤓');
